9df5f74c5c0589ab9ff5c00b0a12e3383cf2d0a1
Bryan Hyland <bryan.hyland32@gmail.com>
2025-11-05 09:11:56 -0800

    Fix make_commits.sh

--- make_commits.sh
+++ make_commits.sh
@@ -7,9 +7,11 @@ repo_root=$(pwd)
 for pkg in $(git status | grep modified | awk '{ print $2 }'); do
   # Change to the top directory in the git status list
   pkg_dir=$(echo $pkg | awk -F'/' '{ print $1"/"$2 }')
+  echo "Changing directory to: {$pkg_dir}"
   cd $pkg_dir
   git add .
   pkg_name=$(echo $pkg | awk -F'/' '{ print $2 }')
   vers=$(cat stone.yaml | grep version | awk '{ print $3 }')
-  cd $cwd
+  git commit -m "${pkg_name}: Update to ${vers}"
+  cd $repo_root
 done
